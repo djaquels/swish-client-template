17367c4ad1467b58c0fa9055592ddc6da9ec500f
Daniel Jacales <hjacales@gmail.com>
2023-11-09 02:22:55 +0100

    ready payments with swish

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const fs = require('fs');
 const https = require('https');
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
+
+
+app.use(express.json())
 
 
 
 const agent = new https.Agent({
-    cert: fs.readFileSync('./.ssl/Swish_Merchant_TestCertificate_1234679304.pem', { encoding: 'utf8' }),
-    key: fs.readFileSync('./.ssl/Swish_Merchant_TestCertificate_1234679304.key', { encoding: 'utf8' }),
-    ca: fs.readFileSync('./.ssl/Swish_TLS_RootCA.pem', { encoding: 'utf8' }),
+    cert: fs.readFileSync('./.ssl/cert.pem', { encoding: 'utf8' }),
+    key: fs.readFileSync('./.ssl/private.key', { encoding: 'utf8' }),
+    //ca: fs.readFileSync('./.ssl/Swish_TLS_RootCA.pem', { encoding: 'utf8' }),
   });
 
 const client = axios.create({
@@ -56,18 +59,18 @@ function getUUID() {
 app.get("/api/payment-request", (req, res) => {
   // Handle payment request logic
   const instructionId = getUUID();
-  const callback = 'https://example.com/swishcallback'
+  const callback = 'https://dance-vida-payments.ey.r.appspot.com/api/callback'
   const data = {
-    payeePaymentReference: '0123456789',
+    payeePaymentReference: '0123456788',
     callbackUrl: callback,
-    payeeAlias: '1234679304',
+    payeeAlias: '1234977948',
     currency: 'SEK',
-    amount: '100',
+    amount: '1',
     message: 'Kingston USB Flash Drive 8 GB'
   };
   
   client.put(
-  `https://mss.cpc.getswish.net/swish-cpcapi/api/v2/paymentrequests/${instructionId}`,
+  `https://cpc.getswish.net/swish-cpcapi/api/v2/paymentrequests/${instructionId}`,
     data
   ).then((api) => {
      //console.log('Payment request created')
@@ -85,8 +88,23 @@ app.get("/api/payment-request", (req, res) => {
 });
 
 app.post("/api/callback", (req, res) => {
+  console.log(req.body)
   res.send("Callback endpoint");
 });
+
+app.get("/api/info", async ( req, res) => {
+  try {
+    const response = await client.get(
+      `https://cpc.getswish.net/swish-cpcapi/api/v1/paymentrequests/D040A107AEEDDEECDE8B0E3A16F9B11F`
+    );
+
+    if (response.status === 200) {
+      return response.data;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+})
 
 // Start the server
 app.listen(PORT, () => {
