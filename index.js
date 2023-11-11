ce85968a0f4347211a5ab6c96dc08c5aaf123fd0
Daniel Jacales <hjacales@gmail.com>
2023-11-11 12:58:57 +0100

    update spreadsheet

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const fs = require('fs');
 const https = require('https');
+const { google } = require('googleapis');
 
 const app = express();
 const PORT = 8080;
@@ -22,28 +23,18 @@ const client = axios.create({
 });
 
 
-async function getQrCodeFromToken(token) {
-  const data = {
-    token,
-    size: "300",
-    format: "png",
-    border: "0"
-  };
+async function _getGoogleSheetClient() {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "./.ssl/sa.json",
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+  });
+  const authClient = await auth.getClient();
+  return google.sheets({
+    version: 'v4',
+    auth: authClient,
+  });
+}
 
-  try {
-    const response = await client.post(
-      `https://mpc.getswish.net/qrg-swish/api/v1/commerce`,
-      data,
-      { responseType: 'arraybuffer' }
-    );
-
-    if (response.status === 200) {
-      return response.data;
-    }
-  } catch (error) {
-    console.error(error);
-  }
-};
 
 function getUUID() {
   const hexDigits = '0123456789ABCDEF';
@@ -54,6 +45,39 @@ function getUUID() {
   }
 
   return key;
+}
+
+async function modifySpreadsheet(reference, status) {
+  const sheets = await await _getGoogleSheetClient();
+  const spreadsheetId = '1EpzPmMtxXkKougfuObdQMav2waeY_yGl6ggkwznQCl8';
+  const range = 'Purchases!A:G'; // Adjust the sheet name and range as per your spreadsheet
+
+  // Read the spreadsheet data
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId,
+    range,
+  });
+  const rows = response.data.values;
+
+  // Find the row with the matching value in column E
+  const rowIndex = rows.findIndex(row => row[4] === `${reference}`); // Assuming column E is index 4
+
+  if (rowIndex !== -1) {
+    // Modify the value in column C for the found row
+    rows[rowIndex][2] = status; // Assuming column C is index 2
+
+    // Update the modified row in the spreadsheet
+    await sheets.spreadsheets.values.update({
+      spreadsheetId,
+      range: `Purchases!C${rowIndex + 1}`, // Assuming column C is index 2
+      valueInputOption: 'RAW',
+      resource: {
+        values: [[rows[rowIndex][2]]],
+      },
+    });
+  } else {
+    console.log('No matching row found.');
+  }
 }
 // Define routes
 app.get("/api/payment-request", (req, res) => {
@@ -100,7 +124,9 @@ app.post("/api/callback", (req, res) => {
     const status = req.body.status
     const payment_reference = req.body.payeePaymentReference
     const update_row = message_maping[status]
-    console.log(`Payment update received: ${payment_reference} ${update_row}`)
+    const reference_clean = payment_reference.replace(/'/g, '');
+    console.log(`Payment update received: ${reference_clean} ${update_row}`);
+    modifySpreadsheet(reference_clean, update_row)
     res.send("Callback endpoint");
   } catch (err) {
     console.log(err)
