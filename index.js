055f775a02c8329436c0b8557c1a50f0949a4b20
Hector Jacales <hjacales@gmail.com>
2025-05-20 14:57:18 +0200

    chore: move gsheets prior code to its own repository

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const https = require('https');
 const path = require('path');
 const dotenv = require('dotenv');
-const { google } = require('googleapis');//move to data  layer
 const { SQLitePaymentsRepository } = require('./data/sqliteRepository.js')
 // dotenv configuraiton
 const env = '.env';
@@ -56,19 +55,6 @@ const getDBConnection = async() => {
 for adding a custom engine please read the docs. README.md
 */
 
-async function _getGoogleSheetClient() {
-  const auth = new google.auth.GoogleAuth({
-    keyFile: "./.ssl/sa.json",
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-  });
-  const authClient = await auth.getClient();
-  return google.sheets({
-    version: 'v4',
-    auth: authClient,
-  });
-}
-
-
 function getUUID() {
   const hexDigits = '0123456789ABCDEF';
   let key = '';
@@ -78,39 +64,6 @@ function getUUID() {
   }
 
   return key;
-}
-
-async function modifySpreadsheet(reference, status) {
-  const sheets = await await _getGoogleSheetClient();
-  const spreadsheetId = '1EpzPmMtxXkKougfuObdQMav2waeY_yGl6ggkwznQCl8';
-  const range = 'Purchases!A:G'; // Adjust the sheet name and range as per your spreadsheet
-
-  // Read the spreadsheet data
-  const response = await sheets.spreadsheets.values.get({
-    spreadsheetId,
-    range,
-  });
-  const rows = response.data.values;
-
-  // Find the row with the matching value in column E
-  const rowIndex = rows.findIndex(row => row[4] === `${reference}`); // Assuming column E is index 4
-
-  if (rowIndex !== -1) {
-    // Modify the value in column C for the found row
-    rows[rowIndex][2] = status; // Assuming column C is index 2
-
-    // Update the modified row in the spreadsheet
-    await sheets.spreadsheets.values.update({
-      spreadsheetId,
-      range: `Purchases!C${rowIndex + 1}`, // Assuming column C is index 2
-      valueInputOption: 'RAW',
-      resource: {
-        values: [[rows[rowIndex][2]]],
-      },
-    });
-  } else {
-    console.log('No matching row found.');
-  }
 }
 
 // Define routes
@@ -155,7 +108,11 @@ app.get("/api/payment-request", async (req, res) => {
     }
   }).catch(err => {
     console.log(err)
-    res.send("Payment Failed")
+    res.send({
+      message: 'Payment request failed!',
+      id: instructionId,
+      error: err
+    })
   })
 });
 
